03687199d7b6e54f1a519269e579c5a85b587ac0
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2020-07-02 07:58:49 -0300

    Criado arquivo loads.js para carregar midia.

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,3 @@
-function preload() {
-    imagemCenario = loadImage("images/cenario/floresta.png");
-    imagemGameOver = loadImage("images/assets/game-over.png");
-    imagemPersonagem = loadImage("images/personagem/correndo.png");
-    imagemInimigo = loadImage("images/inimigos/gotinha.png");
-    imagemInimigoGrande = loadImage("images/inimigos/troll.png");
-    imagemInimigoVoador = loadImage("images/inimigos/gotinha-voadora.png");
-    somDoJogo = loadSound("sounds/trilha_jogo.mp3");
-    somPulo = loadSound("sounds/somPulo.mp3");
-}
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
     cenario = new Cenario(imagemCenario, 3);
